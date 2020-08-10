e7bbdf3c9f825270df06363c64aeab0aac62952f
Lucas Duarte <lucasdrtaraujo@gmail.com>
2020-08-10 10:48:22 -0300

    Add dinamic config for api url and token

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,6 +1,6 @@
 import { InternalError } from '@src/utils/errors/internal-error';
 import { AxiosStatic } from 'axios';
-
+import config, { IConfig } from 'config'
 export interface StormGlassPointSource {
   [key: string]: number;
 }
@@ -43,7 +43,7 @@ export class StormGlassResponseError extends InternalError {
   }
 }
 
-
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass')
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
@@ -56,10 +56,10 @@ export class StormGlass {
     try {
       const response = await this.request.get<StormGlassForecastResponse>(
         `
-        https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}lng=${lng}`,
+        ${stormGlassResourceConfig.get('apiUrl')}/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}lng=${lng}`,
         {
           headers: {
-            Authorization: 'fake-token',
+            Authorization: stormGlassResourceConfig.get('apiToken'),
           },
         }
       );
