4acb7d4e5f482afefebb1063f9445cc838a255e7
Lucas Duarte <lucasdrtaraujo@gmail.com>
2020-08-13 18:38:58 -0300

    Add Database

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import './utils/module-alias'
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
+import { BeachesController } from './controllers/beaches'
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
+import * as database from '@src/database'
 
 export class SetupServer extends Server {
     constructor(private port = 3000) {
         super()
     }
 
-    public init(): void {
+    public async init(): Promise<void> {
         this.setupExpress()
         this.setupController()
+        await this.databaseSetup()
     }
 
     private setupExpress(): void {
@@ -20,7 +23,16 @@ export class SetupServer extends Server {
 
     private setupController(): void {
         const forecastController = new ForecastController()
-        this.addControllers([forecastController])
+        const beachesController = new BeachesController()
+        this.addControllers([forecastController, beachesController])
+    }
+
+    private async databaseSetup(): Promise<void> {
+      await database.connect()
+    }
+
+    public async close(): Promise<void> {
+      await database.close()
     }
 
     public getApp(): Application {
