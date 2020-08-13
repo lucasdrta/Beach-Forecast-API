9228424f35f3f07a97977c574c0cd56b9af23c1e
Lucas Duarte <lucasdrtaraujo@gmail.com>
2020-08-13 18:38:31 -0300

    Add mongodb for tests

--- tests/jest-setup.ts
+++ tests/jest-setup.ts
@@ -1,8 +1,14 @@
-import { SetupServer } from '@src/server'
-import supertest from 'supertest'
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
 
-beforeAll(() => {
-    const server = new SetupServer()
-    server.init()
-    global.testRequest = supertest(server.getApp())
-})
+let server: SetupServer;
+
+beforeAll(async () => {
+  server = new SetupServer();
+  await server.init();
+  global.testRequest = supertest(server.getApp());
+});
+
+afterAll(async () => {
+  await server.close();
+});
