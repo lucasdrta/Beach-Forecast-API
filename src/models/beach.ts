e0691d0a10e3f63721e785ace169ef0664f05e24
Lucas Duarte <lucasdrtaraujo@gmail.com>
2020-08-13 23:50:14 -0300

    Add Beach model

---
+++ src/models/beach.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
+}
+
+export interface Beach {
+  _id?: string;
+  lat: number;
+  lng: number;
+  name: string;
+  position: BeachPosition;
+}
+
+const schema = new Schema(
+  {
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        (ret.id = ret._id), delete ret._id, delete ret.__v;
+      },
+    },
+  }
+);
+
+interface BeachModel extends Omit<Beach, '_id'>, Document {}
+
+export const Beach: Model<BeachModel> = mongoose.model('Beach', schema);
