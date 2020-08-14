aa9481dc2c120f8552b3da144ec4991bd203d778
Lucas Duarte <lucasdrtaraujo@gmail.com>
2020-08-13 23:51:32 -0300

    Add tests for validation

--- tests/functional/beaches.test.ts
+++ tests/functional/beaches.test.ts
@@ -1,4 +1,8 @@
+import { Beach } from '@src/models/beach';
+
 describe('Beaches functional test', () => {
+  beforeAll(async () => await Beach.deleteMany({}));
+
   describe('when creating a beach', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
@@ -12,6 +16,27 @@ describe('Beaches functional test', () => {
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+      });
+    });
+
+    it.skip('should return 500 when there is any error other than validation error', async () => {
+      // To do
     });
   });
 });
