8c2a8b97c81e9cfe51cd6e70c3a7041ad2f47172
Lucas Duarte <lucasdrtaraujo@gmail.com>
2020-08-28 11:52:51 -0300

    Add tests for user controller

--- tests/functional/users.test.ts
+++ tests/functional/users.test.ts
@@ -1,22 +1,53 @@
-import { User } from "@src/models/user"
+import { User } from '@src/models/user';
 
 describe('Users functional tests', () => {
   beforeEach(async () => {
-    await User.deleteMany({})
-  })
+    await User.deleteMany({});
+  });
 
   describe('When creating a user', () => {
     it('should successfully create a new user', async () => {
       const newUser = {
         name: 'John Doe',
         email: 'john@mail.com',
-        password: '1234'
-      }
+        password: '1234',
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newUser = {
+        email: 'john@mail.com',
+        password: '1234',
+      };
 
       const response = await global.testRequest.post('/users').send(newUser)
 
-      expect(response.status).toBe(201)
-      expect(response.body).toEqual(expect.objectContaining(newUser))
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: name: Path `name` is required.'
+      });
     })
-  })
-})
+
+    it('Should return 409 when the email already exists', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await global.testRequest.post('/users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(409);
+      expect(response.body).toEqual({
+        code: 409,
+        error: 'User validation failed: email: already exists in the database.',
+      });
+    });
+  });
+});
