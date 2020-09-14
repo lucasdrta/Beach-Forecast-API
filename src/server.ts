c48fba6f7c3dbfb0560c57fe5e1ea3dc98860a45
Lucas Duarte <lucasdrtaraujo@gmail.com>
2020-09-14 16:16:51 -0300

    Add cors and express-pino middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,58 @@
-import './utils/module-alias'
+import './utils/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
-import { BeachesController } from './controllers/beaches'
+import expressPino from 'express-pino-logger';
+import cors from 'cors';
+import { BeachesController } from './controllers/beaches';
 import { ForecastController } from './controllers/forecast';
-import { UserController } from './controllers/users'
+import { UserController } from './controllers/users';
 import { Application } from 'express';
-import * as database from '@src/database'
+import * as database from '@src/database';
 import logger from './logger';
 
 export class SetupServer extends Server {
-    constructor(private port = 3000) {
-        super()
-    }
+  constructor(private port = 3000) {
+    super();
+  }
 
-    public async init(): Promise<void> {
-        this.setupExpress()
-        this.setupController()
-        await this.databaseSetup()
-    }
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.setupControllers();
+    await this.databaseSetup();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json())
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(expressPino(logger));
+    this.app.use(cors())
+  }
 
-    private setupController(): void {
-        const forecastController = new ForecastController()
-        const beachesController = new BeachesController()
-        const userController = new UserController()
-        this.addControllers([forecastController, beachesController, userController])
-    }
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    const beachesController = new BeachesController();
+    const userController = new UserController();
+    this.addControllers([
+      forecastController,
+      beachesController,
+      userController,
+    ]);
+  }
 
-    private async databaseSetup(): Promise<void> {
-      await database.connect()
-    }
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
 
-    public start(): void {
-      this.app.listen(this.port, () => {
-        logger.info('Server is running on port:' + this.port)
-      })
-    }
+  public start(): void {
+    this.app.listen(this.port, () => {
+      logger.info('Server is running on port: ' + this.port);
+    });
+  }
 
-    public async close(): Promise<void> {
-      await database.close()
-    }
+  public async close(): Promise<void> {
+    await database.close();
+  }
 
-    public getApp(): Application {
-        return this.app
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
